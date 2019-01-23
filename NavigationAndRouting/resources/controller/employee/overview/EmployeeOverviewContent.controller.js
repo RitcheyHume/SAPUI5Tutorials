d79d88c3f6e758eae37d03941b16a763f350c44b
XSA_DEV <XSA_DEV@unknown>
2019-01-23 18:39:32 -0500

    Step 14 (Make Dialogs Bookmarkable) completed in Navigation and Routing

--- NavigationAndRouting/resources/controller/employee/overview/EmployeeOverviewContent.controller.js
+++ NavigationAndRouting/resources/controller/employee/overview/EmployeeOverviewContent.controller.js
@@ -29,10 +29,19 @@ sap.ui.define([
 				this._applySearchFilter(this._oRouterArgs.query.search);
 				// Sorting via URL hash
 				this._applySorter(this._oRouterArgs.query.sortField, this._oRouterArgs.query.sortDescending);
+				// Show dialog via URL hash
+				// show dialog via URL hash
+				if (!!this._oRouterArgs.query.showDialog) {
+					this._oVSD.open();
+					// make sure the dialog does not get closed automatically
+					oEvent.preventDefault();
+				}
 			}
 		},
 		onSortButtonPressed : function (oEvent) {
-			this._oVSD.open();
+			var oRouter = this.getRouter();
+			this._oRouterArgs.query.showDialog = 1;
+			oRouter.navTo("employeeOverview",this._oRouterArgs);
 		},
 		onSearchEmployeesTable : function (oEvent) {
 			var sQuery = oEvent.getSource().getValue();
@@ -45,6 +54,10 @@ sap.ui.define([
 					var oSortItem = oEvent.getParameter("sortItem");
 					this._oRouterArgs.query.sortField = oSortItem.getKey();
 					this._oRouterArgs.query.sortDescending = oEvent.getParameter("sortDescending");
+					oRouter.navTo("employeeOverview",this._oRouterArgs, true /*without history*/);
+				}.bind(this),
+				cancel : function (oEvent){
+					delete this._oRouterArgs.query.showDialog;
 					oRouter.navTo("employeeOverview",this._oRouterArgs, true /*without history*/);
 				}.bind(this)
 			});

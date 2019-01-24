1a758f5a570745968d485f018198bf28e3cd0c58
XSA_DEV <XSA_DEV@unknown>
2019-01-23 20:05:43 -0500

    Step 16 (Handle Invalid Hashes by Listening to Bypassed Events) completed in Navigation and Routing.  The same correction for the log level as in Step 15 applies here, too.

--- NavigationAndRouting/resources/controller/App.controller.js
+++ NavigationAndRouting/resources/controller/App.controller.js
@@ -1,30 +1,26 @@
 sap.ui.define([
 	"sap/ui/demo/nav/controller/BaseController"
-], function (BaseController) {
-	"use strict";
-
-	return BaseController.extend("sap.ui.demo.nav.controller.App", {
-
-		onInit: function () {
-			var oRouter = this.getRouter();
-			oRouter.attachBypassed(function (oEvent) {
-				var sHash = oEvent.getParameter("hash");
-				// do something here, i.e. send logging data to the back end for analysis
-				// telling what resource the user tried to access...
-				//
-				// The simple statement commented out below from the tutorial does not work because the default
-				// log level in SAPUI5 is ERROR.  You have to change the level to INFO, then issue the call 
-				// to jQuery.sap.log.info, and then change the level back to ERROR.  Without changing the level
-				// to INFO, the message simply doesn't show up in the browser console.
-				//
-				// See https://pravin517.wordpress.com/2017/12/25/using-jquery-sap-log-info/ for details.
-				//
-				jQuery.sap.log.setLevel((jQuery.sap.log.Level.INFO));
-				jQuery.sap.log.info("Sorry, but the hash '" + sHash + "' is invalid.", "The resource was not found.");
-				jQuery.sap.log.setLevel((jQuery.sap.log.Level.ERROR));				
-				// jQuery.sap.log.info("Sorry, but the hash '" + sHash + "' is invalid.", "The resource was not found.");
-			});
-		}
+], function(BaseController) {
+		return BaseController.extend("sap.ui.demo.nav.controller.App", {
+			onInit: function() {
+						var oRouter = this.getRouter();
+						oRouter.attachBypassed(function (oEvent) {
+							var sHash = oEvent.getParameter("hash");
+							// do something here, i.e. send logging data to the back end for analysis
+							// telling what resource the user tried to access...
+							//
+							// The simple statement commented out below from the tutorial does not work because the default
+							// log level in SAPUI5 is ERROR.  You have to change the level to INFO, then issue the call 
+							// to jQuery.sap.log.info, and then change the level back to ERROR.  Without changing the level
+							// to INFO, the message simply doesn't show up in the browser console.
+							//
+							// See https://pravin517.wordpress.com/2017/12/25/using-jquery-sap-log-info/ for details.
+							//
+							jQuery.sap.log.setLevel((jQuery.sap.log.Level.INFO));
+							jQuery.sap.log.info("Sorry, but the hash '" + sHash + "' is invalid.", "The resource was not found.");
+							jQuery.sap.log.setLevel((jQuery.sap.log.Level.ERROR));				
+							// jQuery.sap.log.info("Sorry, but the hash '" + sHash + "' is invalid.", "The resource was not found.");
+						});
+			}
+		});
 	});
-});
-
